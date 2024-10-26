be7b619b9ceb36c8d41f6cdc1e66baaed71e87b1
aseneca <princeosuh@gmail.com>
2024-10-26 22:28:14 +0100

    read data from TCP with buf_reader

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
-use std::net::TcpListener;
+
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+    
 
 
 fn main(){
     let addr:String=String::from("127.0.0.1:7878");
+    //returns a new TCPListener Instance
     let listener=TcpListener::bind(addr).unwrap();
     println!("Listener obj{:?}", listener);
 
+    //represents an open conn between client server
     for stream in listener.incoming(){
             let stream=stream.unwrap();
 
-            println!("connection established")
+            handle_connection(stream);
     }
+}
+
+//read data from the TCP stream and print it to inspect data being sent from the browser
+fn handle_connection(mut stream:TcpStream){
+    let buf_reader=BufReader::new(&mut stream);
+    let https_request:Vec<_>=buf_reader
+    .lines()
+    .map(|result|result.unwrap()).
+    take_while(|line|!line.is_empty())
+    .collect();
 }

50b44bfa4d2181ec3e172fa016facc19eac9aaaa
aseneca <princeosuh@gmail.com>
2024-10-29 23:57:45 +0100

    added thread

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
     
 
@@ -15,8 +17,10 @@ fn main(){
     //represents an open conn between client server
     for stream in listener.incoming(){
             let stream=stream.unwrap();
-
-            handle_connection(stream);
+            thread::spawn(||{
+                handle_connection(stream);
+            });
+            
     }
 }
 
@@ -25,11 +29,16 @@ fn handle_connection(mut stream:TcpStream){
     let buf_reader=BufReader::new(&mut stream);
     let request_line=buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, filename)= if request_line == "GET / HTTP/1.1"{
-        ("HTTP/1.1 200 OK", "homepage.html")
-    }else{
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, filename)= match &request_line[..]{
+        "GET / HTTP/1.1"=>("HTTP/1.1 200 OK", "homepage.html"),
+        "GET /sleep HTTP/1.1"=>{
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "homepage.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
+    
+   
     
     let contents= fs::read_to_string(filename).unwrap();
     let length=contents.len();

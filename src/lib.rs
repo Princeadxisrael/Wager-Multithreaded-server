539db1021d887ca58cad784be113179d9ff776b7
aseneca <princeosuh@gmail.com>
2024-11-04 12:54:40 +0100

    created new channel for pool

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-use std::thread;
+use std::{sync::mpsc, thread};
 
 pub struct ThreadPool{
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>
 }
+
+struct  Job;
 
 impl ThreadPool{
    //creates a new threadpool
@@ -14,12 +17,15 @@ impl ThreadPool{
    /// The `new` function will panic if the size is zero
     pub fn new(size:usize)->ThreadPool{
     assert!(size>0);
+
+        //create a new channel 
+        let (sender, reciever)= mpsc::channel();
         let mut workers= Vec::with_capacity(size);
         for id in 0..size{
-         workers.push(Worker::new(id));
+         workers.push(Worker::new(id, reciever));
         }
 
-        ThreadPool{workers}
+        ThreadPool{workers, sender}
     }
     //send transfer the closure from one thread to another and 'static
     //because we don't know how long it will take for the thread to execute
@@ -27,15 +33,17 @@ impl ThreadPool{
     {}
 }
 
-pub struct Worker {
+struct Worker {
     id:usize,
     thread: thread::JoinHandle<()>
 }
 
 impl Worker{
     //fn new takes an id and returns a worker instance that holds the id and a thread spawned with empty closure
-    pub fn new(id:usize)->Worker{
-        let thread=thread::spawn(||{});
+    fn new(id:usize, reciever:mpsc::Receiver<Job>)->Worker{
+        let thread=thread::spawn(||{
+            reciever;
+        });
         Worker{id, thread}
     }
 }
